3e56df6ac711afb6b2f7534d0bad67ef8e7d585d
Karen Lei <karenwinnielei@gmail.com>
2020-06-18 16:38:19 -0700

    animal dashboard

--- animals/src/components/AnimalDashboard.js
+++ animals/src/components/AnimalDashboard.js
@@ -1,19 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import AnimalForm from "./AnimalForm.js";
-import AnimalList from "./AnimalsList.js";
+import AnimalForm from './AnimalForm.js';
+import AnimalList from './AnimalsList.js';
+import { axiosWithAuth } from '../utils/axiosWithAuth.js';
 
 export default function AnimalDashboard() {
-    
-    const [ animals, setAnimals ] = useState([]);
-    
-    // How can get fetch the data from the server when the component mounts?
-    // How can we capture and set that data to state?
+  const [animals, setAnimals] = useState([]);
+  const [update, setUpdate] = useState(false);
 
-    return(
-        <div className="dash">
-            <AnimalForm animals={animals} updateAnimals={setAnimals} />
-            <AnimalList animals={animals} />
-        </div>
-    )
+  // How can get fetch the data from the server when the component mounts?
+  // How can we capture and set that data to state?
+  useEffect(() => {
+    axiosWithAuth()
+      .get('animals')
+      .then((res) => {
+        console.log(res);
+        setAnimals(res.data);
+        setUpdate(false);
+      })
+      .catch((err) => console.log(`There is an error: ${err}`));
+  }, [update]);
+
+  return (
+    <div className='dash'>
+      <AnimalForm animals={animals} updateAnimals={setAnimals} />
+      <AnimalList animals={animals} />
+    </div>
+  );
 }
--- animals/src/components/Login.js
+++ animals/src/components/Login.js
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 export default function Login(props) {
-    // How can we log in? What do we need to do?
+  // How can we log in? What do we need to do?
+  const [login, setLogin] = useState({
+    username: '',
+    password: '',
+  });
 
-    return (
-        <div>
-            <h1>Welcome to the Safari App!</h1>
-            <h2>I can't show you more until you log in. Please build out a login.</h2>
-        </div>
-    )
+  const handleChange = (e) => {
+    e.preventDefault();
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('login', login)
+      .then(res => {
+          console.log(res)
+          localStorage.setItem('token', res.data.payload)
+          props.history.push('/creatures')
+      })
+      .catch((err) => console.log(`Login error: ${err.response}`));
+  };
+  return (
+    <div>
+      <h1>Welcome to the Safari App!</h1>
+      <h2>
+        I can't show you more until you log in. Please build out a
+        login.
+      </h2>
+      <form onSubmit = {handleSubmit}>
+          <input 
+            type = 'text'
+            name = 'username'
+            label = 'username'
+            placeholder = 'username'
+            value = {props.username}
+            onChange={handleChange}
+            className = 'input'
+          />
+          <input 
+            type = 'password'
+            name = 'password'
+            label = 'password'
+            placeholder = 'password'
+            value = {props.password}
+            onChange={handleChange}
+            className = 'input'
+          />
+        <button className='start'>Start</button>
+      </form>
+    </div>
+  );
 }
