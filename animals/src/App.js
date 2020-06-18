40d6e6dcc624505a9897e51d05b8482d0ef2a954
Karen Lei <karenwinnielei@gmail.com>
2020-06-18 15:27:24 -0700

    private route

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from 'react-router-dom';
 
-import Login from "./components/Login.js";
-import Header from "./components/Header.js";
+import Login from './components/Login.js';
+import Header from './components/Header.js';
 
-export default function App() {
-
+const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
       <Switch>
         {/* Build out a Private Route */}
-        <Route exact path="/login" component={Login} />
+        <Route exact path='/login' component={Login} />
       </Switch>
     </div>
   );
 };
+
+export default App;
